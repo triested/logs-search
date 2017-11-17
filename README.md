97a1dd4a9bb8d8fdf5893ac3248764d0cdd14bde
Trieste Desautels <triested@ix-dev.cs.uoregon.edu>
2017-11-17 02:06:11 -0800

    Alter search to allow up to 12 players

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # logs-search
-Search logs.tf for games common to two players
+Search logs.tf for games common to up to 12 players
 
 ## Usage:
 To run, enter the following in a terminal:
 
 	$ python3 main.py
-You'll need the [logs.tf](logs.tf) profiles of the players in question. When prompted, enter their urls. The script will print titles and links for games in which they appear together.
+You'll need the [logs.tf](logs.tf) profiles of the players in question. When prompted, enter their urls, then enter "s" to search. The script will print titles and links for games in which they appear together.
 
-Note: the resulting list of logs is currently limited to the most recent 1000 games of __both__ players.
+Note: the resulting list of logs is currently limited to the most recent 1000 games of __all__ players.
 
 ## Future Plans
     * web interface
-    * searches using any reasonable number of users
+    * remove 1000 game limit
     * search using multiple types of steam identifiers
--- main.py
+++ main.py
@@ -4,6 +4,9 @@ To do:
     - allow searching of a greater number of common players
     - "vanity search" - highest damage differential:
     - incorporate STEAMID conversion (shouldn't require the API)
+    - impose order on results
+    - start to store player aliases, associated with IDs
+    - write test
 Tools:
 Couple of urls:
 http://logs.tf/profile/76561198055233348
@@ -13,43 +16,52 @@ import player
 
 # constants:
 ID64_LENGTH = 17
-MAX_PLAYERS = 2
+MAX_PLAYERS = 12
 
-def get_id64():
-
-  while True:
-    url_input = input("Enter player URL: ")
-    id_index = url_input.find("/profile/")
-    if id_index == -1:
-      print("URL not recognized. Please try again.")
-      continue
-    steam_id_64 = url_input[id_index + 9: id_index + 26]
-    if len(steam_id_64) != 17:
-      print("URL not recognized. Please try again.")
-      continue
-    else:
-      return steam_id_64
-
+# print title and link for logs common to players
 def print_common_logs(players):
-  if len(players) > MAX_PLAYERS:
-    raise RuntimeError("Too many players for now!")
-  for key in players[0].data['logs']:
-    if key in players[1].data['logs']:
-      print(players[1].data['logs'][key]['title'])
-      print("http://logs.tf/" + str(players[1].data['logs'][key]['id']))
+    num_of_players = len(players)
+    if num_of_players > MAX_PLAYERS:
+        raise RuntimeError("Too many players for now!")
+    elif num_of_players == 0:
+        print("No players, no logs.")
+    else:
+        for key in players[0].data['logs']:
+            players_with_key = 1
+            for i in range(1, num_of_players):
+                if key in players[i].data['logs']:
+                    players_with_key += 1
+            if players_with_key == num_of_players:
+                print(players[i].data['logs'][key]['title'])
+                print("http://logs.tf/" + str(players[i].data['logs'][key]['id']))
 
 def main():
-  print("Hi! This script finds urls of logs common to two players.")
-  print("You'll need to provide their two logs.tf pages:")
+    print("Hi! This script finds urls of logs common to up to 12 players.")
+    print("Enter their logs.tf profiles, then enter 's' to search.")
 
-  players = []
+    players = []
+    steam_id_64 = 0
 
-  p1 = player.Player(get_id64())
-  players.append(p1)
-  p2 = player.Player(get_id64())
-  players.append(p2)
+    # get and validate user input for ID
+    while True:
+        url_input = input("Enter player URL: ")
+        if url_input == 's' or url_input == 'S':
+            break
+        # id64 will follow this signature:
+        id_index = url_input.find("/profile/")
+        if id_index == -1:
+            print("Input not recognized. Please try again.")
+            continue
+        # get 17 digits following "/profile/"
+        steam_id_64 = url_input[id_index + 9: id_index + 26]
+        if len(steam_id_64) != 17:
+            print("Input not recognized. Please try again.")
+            continue
+        else:
+            p = player.Player(steam_id_64)
+            players.append(p)
 
-  print_common_logs(players)
+    print_common_logs(players)
 
 if __name__ == "__main__":
-  main()
+    main()
